de7f9e151b7f58b159d8ad60b4b099807805614f
creewick <ilia_Y@mail.ru>
2025-02-13 21:23:33 +0200

    web init

---
+++ web/src/App.tsx
@@ -0,0 +1,89 @@
+import { Route } from 'react-router-dom'
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  setupIonicReact
+} from '@ionic/react'
+import { IonReactRouter } from '@ionic/react-router'
+import { book, dice, home, settings } from 'ionicons/icons'
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css'
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
+
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
+import '@ionic/react/css/palettes/dark.system.css'
+
+/* Theme variables */
+import './theme/variables.css'
+import HomePage from './pages/HomePage'
+import WikiPage from './pages/WikiPage'
+import React from 'react'
+import RoleListPage from './pages/wiki/RoleListPage'
+
+setupIonicReact({ mode: 'ios' })
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <HomePage />
+          </Route>
+          <Route exact path="/wiki">
+            <WikiPage />
+          </Route>
+          <Route path="/wiki/roles">
+            <RoleListPage />
+          </Route>
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={home} />
+            <IonLabel>Home</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="wiki" href="/wiki">
+            <IonIcon icon={book} />
+            <IonLabel>Wiki</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/tab3">
+            <IonIcon icon={dice} />
+            <IonLabel>Table</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon icon={settings} />
+            <IonLabel>Settings</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+)
+
+export default App
