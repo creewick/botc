274953c057a441f329da3e677a048e26243b2b5b
creewick <ilia_Y@mail.ru>
2025-02-13 21:22:19 +0200

    move data to web/public

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -6,13 +6,13 @@ import fetchBotcData from './services/botcParser'
 async function main() {
   dotenv.config()
   const context: Context = {
-    rolesPath: '../data/roles.json',
-    iconsPath: '../data/icons',
+    rolesPath: '../web/public/roles.json',
+    iconsPath: '../web/public/icons',
   }
 
   await runStep('STEP 1: Read Existing Data', readData, context)
   await runStep('STEP 2: Fetch BOTC Data', fetchBotcData, context)
-  await runStep('STEP 3: Save Roles Data', saveData, context)
+  // await runStep('STEP 3: Save Roles Data', saveData, context)
   // await runStep('STEP 2: Fetch Script Tool Data', fetchScriptToolData, context)
   // await runStep('STEP 3: Fetch Wiki Data', fetchWikiData, context)
   // await runStep('STEP 4: Translate Wiki Data', translateRoles, context)
