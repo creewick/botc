1c282a3ef7f4de98770319426bf115b69cead13f
creewick <ilia_Y@mail.ru>
2025-02-14 08:20:43 +0200

    Updates

--- assets/md.transition-legacy-DPDB2Bcy.js
+++ assets/md.transition-legacy-DPDB2Bcy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-0YAqEE7Q.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-B11Hwod1.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
