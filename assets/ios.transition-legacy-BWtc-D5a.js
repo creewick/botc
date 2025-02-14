878a31cd079970aa061822a4e339ff39cc14140b
creewick <ilia_Y@mail.ru>
2025-02-14 12:43:08 +0200

    Updates

--- assets/ios.transition-legacy-BWtc-D5a.js
+++ assets/ios.transition-legacy-BWtc-D5a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-vaPaUj5z.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CFg8x82k.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-UKS-azNU.js
+++ assets/status-tap-legacy-UKS-azNU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-vaPaUj5z.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-CFg8x82k.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
