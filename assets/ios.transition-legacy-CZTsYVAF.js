8ed022f45dce4289ecd37c8499c6faf63aac8326
creewick <ilia_Y@mail.ru>
2025-02-14 16:36:12 +0200

    Updates

--- assets/ios.transition-legacy-CZTsYVAF.js
+++ assets/ios.transition-legacy-CZTsYVAF.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFg8x82k.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BqXAXcht.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
