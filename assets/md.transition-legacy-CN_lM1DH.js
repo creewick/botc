74d881703c45eb7c2d7499befd8b6cefb2730c85
creewick <ilia_Y@mail.ru>
2025-02-17 15:41:13 +0200

    Updates

--- assets/md.transition-legacy-CN_lM1DH.js
+++ assets/md.transition-legacy-CN_lM1DH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DRE_enZq.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BIBa51ZK.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
