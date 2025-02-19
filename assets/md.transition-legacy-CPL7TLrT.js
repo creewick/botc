144b78a45fcafc6a2a2422f6853ebe57fcf9834d
creewick <ilia_Y@mail.ru>
2025-02-19 15:32:26 +0200

    Updates

--- assets/md.transition-legacy-CPL7TLrT.js
+++ assets/md.transition-legacy-CPL7TLrT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-y4hhXQ3e.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-WXXLRuB2.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
