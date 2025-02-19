0870c8733059c27d2f00c2f5ec8fce0205375b51
creewick <ilia_Y@mail.ru>
2025-02-19 23:36:09 +0200

    Updates

--- assets/ios.transition-legacy-_qhR-aBc.js
+++ assets/ios.transition-legacy-_qhR-aBc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CwyYGMC6.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Bhe4BXRU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-Bbgm_stT.js
+++ assets/md.transition-legacy-Bbgm_stT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CwyYGMC6.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Bhe4BXRU.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
