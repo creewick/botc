ac781d048f465d24ef96d01eeb4dfd9fc80874f1
creewick <ilia_Y@mail.ru>
2025-02-20 10:38:35 +0200

    Updates

--- assets/swipe-back-legacy-C27NxQfn.js
+++ assets/swipe-back-legacy-C27NxQfn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BAUfW0XK.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Bjhh6Rzf.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
