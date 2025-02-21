70a672797fa429956c742a1614bff503235145b4
creewick <ilia_Y@mail.ru>
2025-02-21 07:55:53 +0200

    Updates

--- assets/md.transition-legacy-BdCP2mck.js
+++ assets/md.transition-legacy-BdCP2mck.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CMTPLadM.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-u2SFdEH_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
