6ed78c237709d2703fe0c19b77aacfc9638c8e29
creewick <ilia_Y@mail.ru>
2025-02-21 10:27:13 +0200

    Updates

--- assets/noGreaterJoy-legacy-C5tU_yIw.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e,t){"use strict";return{execute:function(){e("default",[{id:"_meta",name:"No Greater Joy",author:"Steven Medway",tags:["teen"],difficulty:{storyteller:1.5,player:1.5}},"clockmaker","investigator","empath","chambermaid","artist","sage","drunk","klutz","scarlet_woman","baron","imp"])}}}));
--- assets/overTheRiver-legacy-Dfo_HJqO.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e,r){"use strict";return{execute:function(){e("default",[{id:"_meta",name:"Over the River",author:"Andrew Nathenson",tags:["teen"],difficulty:{storyteller:2,player:3}},"grandmother","clockmaker","innkeeper","snake_charmer","professor","slayer","lunatic","recluse","godfather","spy","imp"])}}}));
