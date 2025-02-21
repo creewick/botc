4526e117005f96acbeaa45386f7b9a40c0d42fca
creewick <ilia_Y@mail.ru>
2025-02-21 07:36:15 +0200

    Updates

--- assets/ios.transition-legacy-aNJo_yAl.js
+++ assets/ios.transition-legacy-aNJo_yAl.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bjhh6Rzf.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CMTPLadM.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
